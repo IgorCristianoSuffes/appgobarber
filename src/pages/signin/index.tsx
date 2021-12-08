dd545962acff78fb405c875b0865bcce2cd3ff0b
Igor Cristiano Suffes <cristianosuqe@gmail.com>
2021-12-07 21:39:57 -0300

    Criando projeto React Native / Configurando StatusBar / Configurando Navegação

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from  './styles';
+
+const SignIn: React.FC = () => {
+    return (
+        <Container />
+    );
+}
+
+export default SignIn;
+
+// configurando navegação 02:00
