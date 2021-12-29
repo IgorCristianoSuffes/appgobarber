9e0dbacd9642226cca0ebe849b42e232c5e80655
IgorCristianoSuffes <45612189+IgorCristianoSuffes@users.noreply.github.com>
2021-12-28 22:09:59 -0300

    formatado PC android studio não está funcioanando

--- Teste.ts
+++ Teste.ts
@@ -2,4 +2,5 @@
 // problemas com android Studio
 // formatado PC android studio não está funcioanando
 // formatado PC android studio não está funcioanando - 2
-// formatado PC android studio não está funcioanando - 2
+// formatado PC android studio não está funcioanando - 3
+// formatado PC android studio não está funcioanando - 4
