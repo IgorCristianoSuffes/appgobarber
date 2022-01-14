b8f2687a163235b8d591ee445aeb7b4d520c1cef
IgorCristianoSuffes <45612189+IgorCristianoSuffes@users.noreply.github.com>
2022-01-13 21:55:10 -0300

    formatado PC android studio não está funcioanando  13

--- Teste.ts
+++ Teste.ts
@@ -13,3 +13,4 @@
 // formatado PC android studio não está funcioanando - 11
 // formatado PC android studio não está funcioanando - 12
 // formatado PC android studio não está funcioanando - 13
+// formatado PC android studio não está funcioanando - 14
