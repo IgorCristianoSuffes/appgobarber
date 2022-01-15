40e0d35c521076469e1d8f895297741f351695bb
IgorCristianoSuffes <45612189+IgorCristianoSuffes@users.noreply.github.com>
2022-01-14 22:18:41 -0300

    Funcionando upload

--- Teste.ts
+++ Teste.ts
@@ -1,16 +1,16 @@
 // teste conexão
 // problemas com android Studio
-// formatado PC android studio não está funcioanando
-// formatado PC android studio não está funcioanando - 2
-// formatado PC android studio não está funcioanando - 3
-// formatado PC android studio não está funcioanando - 4
-// formatado PC android studio não está funcioanando - 5
-// formatado PC android studio não está funcioanando - 6
-// formatado PC android studio não está funcioanando - 7
-// formatado PC android studio não está funcioanando - 8
-// formatado PC android studio não está funcioanando - 9
-// formatado PC android studio não está funcioanando - 10
-// formatado PC android studio não está funcioanando - 11
-// formatado PC android studio não está funcioanando - 12
-// formatado PC android studio não está funcioanando - 13
-// formatado PC android studio não está funcioanando - 14
+// formatado PC android studio não está funcionando
+// formatado PC android studio não está funcionando - 2
+// formatado PC android studio não está funcionando - 3
+// formatado PC android studio não está funcionando - 4
+// formatado PC android studio não está funcionando - 5
+// formatado PC android studio não está funcionando - 6
+// formatado PC android studio não está funcionando - 7
+// formatado PC android studio não está funcionando - 8
+// formatado PC android studio não está funcionando - 9
+// formatado PC android studio não está funcionando - 10
+// formatado PC android studio não está funcionando - 11
+// formatado PC android studio não está funcionando - 12
+// formatado PC android studio não está funcionando - 13
+// formatado PC android studio não está funcionando - 14
